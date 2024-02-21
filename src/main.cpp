8dc921b77785c24a425823c4c48107a9a6688480
MichalMatu <michal.matuszewski1984@gmail.com>
2024-02-21 16:27:50 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,16 @@ const int BUFFER_SIZE = 128;
 int co2Array[BUFFER_SIZE];
 int co2Index = 0;
 
+// Define circular buffer for temperature values
+const int TEMP_BUFFER_SIZE = 128;
+float tempArray[TEMP_BUFFER_SIZE];
+int tempIndex = 0;
+
+// Define circular buffer for humidity values
+const int HUM_BUFFER_SIZE = 128;
+float humArray[HUM_BUFFER_SIZE];
+int humIndex = 0;
+
 float temperature = 0.0f;
 float humidity = 0.0f;
 
@@ -73,7 +83,7 @@ void setup()
 }
 
 unsigned long previousMillis = 0;
-int currentMenuOption = 1;
+int currentMenuOption = 2;
 
 void loop()
 {
@@ -88,11 +98,14 @@ void loop()
     uint16_t error = scd4x.readMeasurement(tempCO2, tempTemp, tempHum);
     if (!error)
     {
+
       // Move index circularly
       co2Index = (co2Index + 1) % BUFFER_SIZE;
-      // Store average CO2 reading from 1 hour in circular buffer
-
       co2Array[co2Index] = tempCO2;
+      tempIndex = (tempIndex + 1) % TEMP_BUFFER_SIZE;
+      tempArray[tempIndex] = tempTemp;
+      humIndex = (humIndex + 1) % HUM_BUFFER_SIZE;
+      humArray[humIndex] = tempHum;
     }
 
     previousMillis = millis();
@@ -139,6 +152,33 @@ void loop()
     for (int i = 0; i < BUFFER_SIZE; i++)
     {
       display.drawPixel(i, map(co2Array[i], 400, 2000, display.height(), 0), SSD1306_WHITE); // Plot CO2 values
+    }
+    delay(1000);
+    break;
+
+  case 2:
+    display.clearDisplay();
+    // in top left corner draw 24m CO2 graph
+    display.setCursor(0, 10);
+    display.setFont(&Org_01);
+    display.print("24m temperature");
+    // below draw points from left to right representinh 1 hour pass
+    for (int i = 0; i < 128; i = i + 11)
+    {
+      display.drawPixel(i, 15, SSD1306_WHITE); // Plot CO2 values
+    }
+
+    // in top right  corner draw 2000
+    display.setCursor(100, 10);
+    // use tiny font
+    display.setFont(&Org_01);
+    display.print("50");
+    // in bottom right corner draw 400
+    display.setCursor(100, 60);
+    display.print("0");
+    for (int i = 0; i < BUFFER_SIZE; i++)
+    {
+      display.drawPixel(i, map(tempArray[i], 0, 50, display.height(), 0), SSD1306_WHITE); // Plot CO2 values
     }
     delay(1000);
     break;
